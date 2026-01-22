42f3e67e13dbeeb6361b7fe7cc707e43db3327ea
soniagkalliath <soniageorge111@gmail.com>
2026-01-22 15:36:20 +0530

    add: register controller

---
+++ controller/userController.js
@@ -0,0 +1,23 @@
+const users = require('../model/userModel')
+const bcrypt = require('bcrypt')
+
+//register
+exports.registerController = async (req,res)=>{
+    console.log("Iniside registerController");
+    const {username,email,password} = req.body
+    try{
+        const existingUser = await users.findOne({email})
+        if(existingUser){
+            res.status(409).json("User already Exists... Please Login!!!")
+        }else{
+            let encryptedPassword = await bcrypt.hash(password,10)
+            const newUser = await users.create({
+                username,email,password:encryptedPassword
+            })
+            res.status(200).json(newUser)
+        }
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error)
+    }
+}
--- routes.js
+++ routes.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const recipeController = require('./controller/recipeController')
+const userController = require('./controller/userController')
 
 const router = new express.Router()
 
 // routes
 router.get('/recipes',recipeController.getAllRecipesController)
+//register
+router.post('/register',userController.registerController)
 
 module.exports = router
